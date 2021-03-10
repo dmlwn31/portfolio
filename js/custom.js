3f3f8346fe68d9782e315ec26f645a7c8bc127b0
dmlwn31 <dmlwn31@gmail.com>
2021-03-10 18:38:31 +0900

    section1 메인페이지 디자인 완료

--- js/custom.js
+++
@@ -1,6 +0,0 @@
-new fullScroll({
-  displayDots: true,
-  dotsPosition: 'left',
-  animateTime: 0.7,
-  animateFunction: 'ease'
-});
---
+++ js/page_scroll.js
@@ -0,0 +1,50 @@
+window.onload = function () {
+  $('.sectionBox').eq(0).addClass('move');
+    var elm = ".sectionBox";
+    $(elm).each(function (index) {
+        // 개별적으로 Wheel 이벤트 적용
+        $(this).on("mousewheel DOMMouseScroll", function (e) {
+            e.preventDefault();
+            var delta = 0;
+            if (!event) event = window.event;
+            if (event.wheelDelta) {
+                delta = event.wheelDelta / 120;
+                if (window.opera) delta = -delta;
+            } 
+            else if (event.detail)
+                delta = -event.detail / 3;
+            var moveTop = $(window).scrollTop();
+            var elmSelecter = $(elm).eq(index);
+            // 마우스휠을 위에서 아래로
+            if (delta < 0) {
+                if ($(elmSelecter).next() != undefined) {
+                    try{
+                        moveTop = $(elmSelecter).next().offset().top;
+                        $(elmSelecter).next().addClass('move'); //  휠 내렸을시 애니메이션 이벤트
+                    }catch(e){}
+                }
+            // 마우스휠을 아래에서 위로
+            } else {
+                if ($(elmSelecter).prev() != undefined) {
+                    try{
+                        moveTop = $(elmSelecter).prev().offset().top;
+                        
+                    }catch(e){}
+                }
+            }
+             
+            // 화면 이동 0.8초(800)
+            $("html,body").stop().animate({
+                scrollTop: moveTop + 'px'
+            }, {
+                duration: 800, complete: function () {
+                }
+            });
+        });
+    });
+    
+    $(window).resize(function(){
+      $('.sectionBox').css('height',$(window).height());
+    });
+    
+}
